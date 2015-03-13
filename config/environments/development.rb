96d4d51b6d2e89d53b5f9c8d66b9402d5c7e7585
abesamislyndon <abesamislyndon@gmail.com>
2015-03-13 11:13:04 +0800

    forget add production environment config assets to true

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,8 @@ Rails.application.configure do
   config.serve_static_assets = true
   config.assets.compile = true
 
-  # Adds additional error checking when serving assets at runtime.
+
+ # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
