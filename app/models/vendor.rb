8ac8e5e7ce4dc2ccb36081a28be70ef95a91d89e
dondon <abesamislyndon@gmail.com>
2015-03-13 08:09:26 -0700

    fix signup failed to insert in database

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,5 +1,5 @@
 class Vendor < ActiveRecord::Base
-    self.inheritance_column = nil
+  self.inheritance_column = nil
   has_attached_file :image, styles: { large: "600x600>", medium: "310x350>",  thumb:"150x150#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
