e5666b26b43851433a65bd0d358b172b64df41fa
DONDON <abesamislyndon@gmail.com>
2015-04-09 17:07:57 +0800

    fix again redirection prblem in heroku 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { :omniauth_callbacks => 'authentications', :registrations => 'registrations' }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
    resources :vendors, :only => [:new, :result_page, :create]
    resources :admin_page, :only => [:vendor_list, :destroy, :update]
 
