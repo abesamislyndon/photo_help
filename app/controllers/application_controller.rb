e7507b82972bc9090b1aac186575259883299ffb
DONDON <abesamislyndon@gmail.com>
2015-04-09 16:10:48 +0800

    update redirection after login af fb succesfully login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,11 @@ class ApplicationController < ActionController::Base
    vendors_new_path
   end
 
+def after_sign_in_path_for(resource_or_scope)
+   if request.env['omniauth.origin']
+      request.env['omniauth.origin']
+    end
+end
+
+
 end
